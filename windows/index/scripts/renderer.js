e7392ac23537b592a2163a6c8a3d4d6b84ab61b0
roikramer120 <roikramer120@gmail.com>
2017-07-28 20:04:45 +0300

    changed everything, added window controls buttons

--- windows/index/scripts/renderer.js
+++ windows/index/scripts/renderer.js
@@ -4,3 +4,16 @@ function closeWindow(){
     var window = remote.getCurrentWindow();
     window.close();
 }
+
+function maximizeWindow(){
+    var window = remote.getCurrentWindow();
+    if(window.isMaximized())
+        window.unmaximize();
+    else
+        window.maximize();
+}
+
+function minimizeWindow(){
+    var window = remote.getCurrentWindow();
+    window.minimize();
+}
